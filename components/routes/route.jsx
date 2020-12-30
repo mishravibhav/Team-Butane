87266e1fdd3417bb828dc763e07a94c674f9eed9
simha <bhupendrasimha@gmail.com>
2020-12-30 12:01:28 +0530

    Routing and work

---
+++ components/routes/route.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+ import{Route,Switch} from "react-router-dom"
+import Navbar from "../pages/navbar"
+
+
+import Working from "../pages/work"
+
+
+const Routing =()=>{
+    return(
+        <div>
+            <div>
+            <Navbar/>
+            </div>
+          
+         
+            <Switch>
+                <Route exact path="/how-it-works" render={()=><Working/>}  />
+ 
+                
+            </Switch>
+            
+        </div>
+    )
+}
+export default Routing
