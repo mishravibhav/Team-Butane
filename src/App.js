aa2b146bbbed5fe316342ca945bd3d0089e8a1b5
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2020-12-30 15:18:34 +0530

    merge swapnil Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Work from "./Components/routes/route"
+import Home from "./Components/pages/Homepage/Home"
 function App() {
   return (
     <div>
      <Work/>
+     <Home/>
     </div>
   );
 }
